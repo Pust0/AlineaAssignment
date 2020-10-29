747675bd3bcf9b2b59a8a06a948c5744dd7f0ffd
Varun <varunbhouria78@gmail.com>
2020-10-29 20:09:11 +0530

    navbar final done

--- internshipassignment/src/Navbar.js
+++ internshipassignment/src/Navbar.js
@@ -4,14 +4,14 @@ import logo from "./Assets/icon.png"
 
 function Navbar(){
    return (
-      <div className="navbar">
+      <header>
          <img className="logo" src={logo} alt="Logo" />
          <a href="#" className="LogoHeading">ALINEA INVEST</a>
          <ul className="navbarList">
             <li><a href="#">Login</a></li>
             <li><a href="#">Join Now</a></li>
          </ul>
-      </div>
+      </header>
    )
 }
 
